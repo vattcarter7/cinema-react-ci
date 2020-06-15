356525e906a6e599c0e1acc9fb94d9362ff7922b
vattcarter7 <vattcarter@gmail.com>
2020-06-15 17:21:11 +0700

    added slideshow component

---
+++ src/components/content/main-content/MainContent.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import SlideShow from '../slide-show/SlideShow';
+
+import './MainContent.scss';
+
+const MainContent = () => {
+  const images = [
+    {
+      url: 'https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&w=1000&q=80'
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&w=1000&q=80'
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&w=1000&q=80'
+    }
+  ];
+
+  return (
+    <div className="main-content">
+      <SlideShow images={images} auto={true} showArrows={true} />
+      <div className="grid-movie-title">
+        <div className="movieType">Now Playing</div>
+        <div className="paginate">Paginate</div>
+      </div>
+    </div>
+  );
+};
+
+export default MainContent;
